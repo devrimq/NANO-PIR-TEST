78e7d3e14f257aeeab6123a905ef619a719c32f2
Devrim Sarıkaya <devrimq@gmail.com>
2026-06-30 17:43:59 +0300

    Implement advanced PIR motion detection with LED

--- Arduino-HC-SR501.ino
+++ Arduino-HC-SR501.ino
@@ -1,1 +1,45 @@
+// Gelişmiş PIR Hareket Dedektörü
 
+int pirPin = 2;      // PIR sensörü OUT pini
+int ledPin = 3;      // LED pini
+int pirState = LOW;  
+unsigned long hareketBaslangic = 0;
+bool hareketVar = false;
+
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Gelişmiş PIR Sensörü Başlatıldı!");
+}
+
+void loop() {
+  pirState = digitalRead(pirPin);
+  
+  if (pirState == HIGH && !hareketVar) {
+    // Yeni hareket algılandı
+    hareketVar = true;
+    hareketBaslangic = millis();
+    digitalWrite(ledPin, HIGH);
+    Serial.println("🔴 HAREKET ALGILANDI!");
+    
+    // Yanıp sönme efekti (3 kez)
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(ledPin, HIGH);
+      delay(200);
+      digitalWrite(ledPin, LOW);
+      delay(200);
+    }
+    digitalWrite(ledPin, HIGH); // Son halinde yanık kalsın
+    
+  } else if (pirState == LOW && hareketVar) {
+    // Hareket sona erdi - 5 saniye sonra LED'i söndür
+    if (millis() - hareketBaslangic > 5000) {
+      hareketVar = false;
+      digitalWrite(ledPin, LOW);
+      Serial.println("⏰ Hareket sona erdi, LED söndü");
+    }
+  }
+  
+  delay(50);
+}
